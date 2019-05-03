2cb7536e073ef5e078340dc84fa002986c4e5608
Blooker20 <jesper.engberg@gmx.at>
2019-05-03 15:02:44 +0200

    Dashboard Structure

--- sites/dashboard/dashboard.php
+++ sites/dashboard/dashboard.php
@@ -1,5 +1,16 @@
 <?php
-  if(checkAccess()) {
-    
+  if(!checkAccess()) {
+    return;
   }
 ?>
+<div id="dashboard">
+  <div class="container">
+    <div class="elemente">
+      <div class="element c-1">
+        <div class="head">
+          Kopf
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
